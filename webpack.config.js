b4904fb97d1a15cbdc8a369eaefd06f22a264de8
Stuart Urback <stuart@urback.net>
2016-04-04 16:34:11 -0600

    Manage Package Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const NpmInstallPlugin = require('npm-install-webpack-plugin')
 
+const pkg = require('./package.json')
+
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
@@ -25,7 +27,7 @@ const common = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
@@ -89,6 +91,11 @@ if (TARGET === 'start' || !TARGET){
 
 if(TARGET === 'build'){
   module.exports = merge(common, {
+    entry : {
+      vendor: Object.keys(pkg.dependencies).filter(function(v){
+        return v !== 'alt-utils'
+      })
+    },
     plugins: [
       new webpack.DefinePlugin({
         'process.env.NODE_ENV' : '"production"'
